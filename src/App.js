a2f512ecb185dd359d0595aa55c5c4ee6ade9670
Matias <lijuegos@hotmail.es>
2020-08-06 20:45:35 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
 
 function App() {
-  return (
+  const printAll = () =>{
+    // con stringify
+    //console.log(JSON.stringify(data));
+
+    data.coches.map(coche =>
+      console.log("Marca: ", coche.marca +" // ", "Modelo", coche.modelo + " // ", coche.puertas ? 
+      "Puertas " + coche.puertas + " // Precio " + coche.precio
+      : 
+      "Cilindrada " + coche.cilindrada + " // Precio " + coche.precio )      
+      )
+  }
+  const printMax = () => {
+    const max = data.coches.reduce(function (prev, current) {
+      return parseFloat(prev.precio) > parseFloat(current.precio)
+        ? prev
+        : current;
+    });
+    console.log(max.marca);
+  };
+  
+  const printMin = () => {
+    const min = data.coches.reduce(function (prev, current) {
+      return parseFloat(prev.precio) < parseFloat(current.precio)
+        ? prev
+        : current;
+    });
+    console.log(min.marca);
+  };
+  
+  const modelByLetter = () => {
+    for (let i = 0; i < data.coches.length; i++) {
+      if (data.coches[i].marca.charAt(0) === 'Y') {
+        console.log(data.coches[i].modelo + ' ' + data.coches[i].precio);
+        break;
+      }
+    }
+  };
+  return (  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Testing app</h1>
+        {printAll()}
+        {printMax()}
+      {printMin()}
+      {modelByLetter()}
       </header>
+      <div></div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default {
+    coches: [
+      {
+        _id: '1',
+        marca: 'Peugeot',
+        modelo: '206',
+        puertas: 4,
+        precio: "200.000,00",     
+        cilindrada:null,
+   
+      },
+      {
+        _id: '2',
+        marca: 'Honda',
+        modelo: 'Titan',
+        puertas: null,
+        cilindrada:'125c',
+        precio: "60.000",        
+      },
+      {
+        _id: '3',
+        marca: 'Peugeot',
+        modelo: '208',
+        puertas: 5,
+        precio: "250.000,00",    
+      },
+      {
+        _id: '4',
+        marca: 'Yamaha',
+        modelo: 'YBR',
+        puertas: null,
+        cilindrada:'160c',
+        precio: "80.500,50",        
+      },
+      
+    ]
+    
+  }
